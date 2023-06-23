3e8b3db1a2121795cbf623f535b005dd1563168d
Emaya009 <131105792+Emaya009@users.noreply.github.com>
2023-06-23 17:27:45 -0400

    Update Prescription_itemsService.java

--- services/Prescription_itemsService.java
+++ services/Prescription_itemsService.java
@@ -14,7 +14,7 @@ import java.io.Reader;
 import java.sql.SQLException;
 import java.util.List;
 
-public class Prescription_itemsService {
+public class Prescription_itemsService implements Prescription_itemsMapperDAO {
     Logger logger = LogManager.getLogger(Prescription_itemsService.class.getName());
     private SqlSessionFactory sqlSessionFactory;
 
